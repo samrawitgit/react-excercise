ec7ca6e82c7870b7af9276608048c833a35aa3e8
@stekiefassil <samrawit.a.tekie-fassil@capgemini.com>
2023-12-19 11:46:14 +0100

    Add timeout cleanup

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-export const Main = (props) => {
+export const Main = () => {
   const [classNames, setClassNames] = useState("square");
+  const [isTimeoutActive, setIsTimeoutActive] = useState(false);
 
   const onClick = () => {
     setClassNames((prev) => prev + " xl");
-    setTimeout(() => setClassNames("square"), 2000);
+    setIsTimeoutActive(true);
   };
 
+  useEffect(() => {
+    if (isTimeoutActive) {
+      const timeout = window.setTimeout(() => {
+        setClassNames("square");
+        setIsTimeoutActive(false);
+      }, 2000);
+      return () => {
+        window.clearTimeout(timeout);
+      };
+    }
+  }, [isTimeoutActive]);
+
+  console.log({ isTimeoutActive });
   return (
     <>
       <h1>Hi Cathy!</h1>
